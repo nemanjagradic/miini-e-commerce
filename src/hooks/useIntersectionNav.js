cd998bed4371bee6ee72d7678e628bfd18489b40
nemanjagradic <nemanjagradic12345@gmail.com>
2023-08-07 21:49:12 +0200

    fixed small issue

--- src/hooks/useIntersectionNav.js
+++ src/hooks/useIntersectionNav.js
@@ -1,27 +1,30 @@
-import { useMemo, useEffect } from "react";
+import { useMemo, useEffect, useCallback } from "react";
 
 const useIntersectionNav = (options, targetEl, categoryPage = false) => {
   // nisam znao na koji drugi nacin da selektujem nav, pa sam uradio na ovaj nacin
   const nav = document.querySelector(".nav");
 
-  const obsCallback = function (entries) {
-    const [entry] = entries;
+  const obsCallback = useCallback(
+    (entries) => {
+      const [entry] = entries;
 
-    if (categoryPage && entry.intersectionRect.y > 15) {
-      nav.classList.remove("sticky");
-    }
-    if (categoryPage && entry.intersectionRect.y < 15) {
-      nav.classList.add("sticky");
-    }
-
-    if (!categoryPage) {
-      if (!entry.isIntersecting) {
-        nav.classList.add("sticky");
-      } else {
+      if (categoryPage && entry.intersectionRect.y > 15) {
         nav.classList.remove("sticky");
       }
-    }
-  };
+      if (categoryPage && entry.intersectionRect.y < 15) {
+        nav.classList.add("sticky");
+      }
+
+      if (!categoryPage) {
+        if (!entry.isIntersecting) {
+          nav.classList.add("sticky");
+        } else {
+          nav.classList.remove("sticky");
+        }
+      }
+    },
+    [nav.classList, categoryPage]
+  );
 
   const obsOptions = useMemo(() => {
     return options;
