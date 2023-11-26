97570180701d6659a2db37c41a97920210eec8ac
nemanjagradic <nemanjagradic12345@gmail.com>
2023-11-26 20:29:30 +0100

    Small change

--- src/components/Products/ProductSmallItem.js
+++ src/components/Products/ProductSmallItem.js
@@ -4,7 +4,7 @@ const ProductSmallItem = ({ product }) => {
   return (
     <div className="mb-5 border border-solid transition duration-400 hover:shadow-big border-black/20 hover:border-black/40">
       <Link to={`/products/${product.id}`}>
-        <div className="w-56 h-56 md:w-[270px] md:h-[250px]">
+        <div className="w-full h-56 md:w-[270px] md:h-[250px]">
           <img className="w-full h-full" src={`/${product.imgs[0]}`} alt="" />
         </div>
         <h3 className="font-Heebo text-2xl m-2.5">{product.title}</h3>
