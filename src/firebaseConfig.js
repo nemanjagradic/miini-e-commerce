f207713bfc603aae02332cc98f013d9e711fcefa
nemanjagradic <nemanjagradic12345@gmail.com>
2024-03-11 22:46:59 +0100

    Upgraded form with submitting to firebase and message

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCE5Bjfr8MtEjCn1c0h7ir7DLT8vDpkYZE",
@@ -14,4 +13,3 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
