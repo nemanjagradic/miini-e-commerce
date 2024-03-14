078f055dc66bf60af226c81f1c875c8e6a94296b
nemanjagradic <nemanjagradic12345@gmail.com>
2024-03-14 16:18:04 +0100

    Added responsive to checkout page three

--- src/components/Layout/Checkout/CheckoutStepThree.js
+++ src/components/Layout/Checkout/CheckoutStepThree.js
@@ -6,7 +6,7 @@ const CheckoutStepThree = () => {
   const location = useLocation();
   const orderId = new URLSearchParams(location.search).get("id");
   return (
-    <div className="m-auto mt-24 w-1/3 bg-green-400/70 px-10 py-5 text-center">
+    <div className="m-auto mt-24 w-[260px] bg-green-400/70 px-5 py-5 text-center min-[400px]:w-[350px] min-[400px]:px-10 md:w-[500px]">
       <div className="mb-4 inline-block rounded-full bg-green-500 px-2.5 py-1.5">
         <FontAwesomeIcon className="text-xl text-white" icon={faCheck} />
       </div>
